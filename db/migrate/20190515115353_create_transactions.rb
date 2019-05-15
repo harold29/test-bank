e97f80d9d3428576ef2977accde615b4cb65cae8
Harold Perez <haroldjose2903@gmail.com>
2019-05-15 14:49:16 -0300

    chore(transaction migration): change type for status and action in transaction migration

--- db/migrate/20190515115353_create_transactions.rb
+++ db/migrate/20190515115353_create_transactions.rb
@@ -2,8 +2,8 @@ class CreateTransactions < ActiveRecord::Migration[5.2]
   def change
     create_table :transactions do |t|
       t.references :user
-      t.string :status
-      t.string :action
+      t.integer :status
+      t.integer :action
       t.float :amount
       t.references :origin_account
       t.references :destination_account
