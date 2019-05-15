6ec28d89fb24b3223215f9e4150b0d829b2d5a90
Harold Perez <haroldjose2903@gmail.com>
2019-05-15 09:00:54 -0300

    fix(transaction migration): fix typos and run migration

--- db/migrate/20190515115353_create_transactions.rb
+++ db/migrate/20190515115353_create_transactions.rb
@@ -1,10 +1,10 @@
 class CreateTransactions < ActiveRecord::Migration[5.2]
   def change
     create_table :transactions do |t|
-      t.references, :user
-      t.string, :status
-      t.string, :action
-      t.float, :amount
+      t.references :user
+      t.string :status
+      t.string :action
+      t.float :amount
       t.references :origin_account
       t.references :destination_account
 
