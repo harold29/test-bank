193c48d3a844444386e1b0c28d94008df67cbea0
Harold Perez <haroldjose2903@gmail.com>
2019-05-17 10:32:57 -0300

    feat(routes): add routes for accounts, deposit and transfer

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "accounts#show"
-  resource :accounts
-  post 'transfer', to: 'accounts#transfer' 
+  resources :accounts
+  post 'transfer', to: 'accounts#transfer'
+  post 'deposit', to: 'accounts#deposit'
 end
